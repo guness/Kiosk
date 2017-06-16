8ed7169c52eedd0fd76ab0112b2dc44ff83486b0
Sinan Güneş <gunes.sinan@gmail.com>
2017-06-16 07:55:14 +0300

    VideoView seems cool

--- app/src/main/java/com/guness/kiosk/pages/MainActivity.java
+++ app/src/main/java/com/guness/kiosk/pages/MainActivity.java
@@ -4,17 +4,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.widget.VideoView;
 
 import com.guness.kiosk.R;
 import com.guness.kiosk.core.BaseActivity;
 import com.guness.kiosk.services.CardReaderService;
 import com.guness.kiosk.services.OverlayService;
 
+import butterknife.BindView;
+
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.videoView)
+    VideoView mVideoView;
 
     private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -27,6 +34,13 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO, make this configurable
+        mVideoView.setVideoURI(Uri.parse(getString(R.string.url_movie)));
+        mVideoView.setOnPreparedListener(mediaPlayer -> {
+            mVideoView.start();
+            mediaPlayer.setLooping(true);
+        });
+
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, new IntentFilter(CardReaderService.ACTION_CARD_ATTACHED));
     }
 
@@ -39,6 +53,7 @@ public class MainActivity extends BaseActivity {
     protected void onPause() {
         super.onPause();
         if (isFinishing()) {
+            mVideoView.stopPlayback();
             LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
         }
     }
