2f00985db1c9b2c8db86b0cb6a9434c073ab9c78
Sinan Güneş <gunes.sinan@gmail.com>
2017-06-28 00:48:06 +0300

    Fabric installed to kiosk

--- Service/src/main/java/com/guness/kiosk/service/core/ServiceApplication.java
+++ Service/src/main/java/com/guness/kiosk/service/core/ServiceApplication.java
@@ -2,8 +2,10 @@ package com.guness.kiosk.service.core;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.firebase.database.FirebaseDatabase;
 import com.guness.kiosk.service.receivers.BootReceiver;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by guness on 11/06/2017.
@@ -13,6 +15,7 @@ public class ServiceApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         BootReceiver.startServices(this);
     }
